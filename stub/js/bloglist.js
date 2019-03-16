6b866c594d7a2087a05b150e30f7e34a2613ade6
Pankaj <pankajverma2378@gmai.com>
2019-03-16 23:14:14 +0530

    Part B completed

---
+++ stub/js/bloglist.js
@@ -0,0 +1,9 @@
+var deleteM = document.getElementById('deleteModalInfo');
+deleteM.style.display = "none";
+function deleteModal(){
+    deleteM.style.display= "flex";
+}
+
+function hideDeleteModal(){
+    deleteM.style.display = "none";
+}
